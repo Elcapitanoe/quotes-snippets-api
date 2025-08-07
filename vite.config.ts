5d8a13f28995db25a6f6f8839ab2224e9c62b2e4
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-08-07 23:02:53 +0700

    Fix(vite): Correct missing parenthesis in config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    target: 'esnext',
+    target: 'es2015',
     rollupOptions: {
       input: './index.html'
-    }
+    },
+    assetsDir: 'assets'
   },
   server: {
     port: 3000,
     open: true
-  }
+  },
+  base: './',
+  publicDir: 'public'
 })
